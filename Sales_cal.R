d067de3e9b237527b1500b8cb6a189b1bd5e46c9
Huong Vo <helentmvo@gmail.com>
2021-10-27 12:25:39 -0700

    Create Sales_cal.R

---
+++ Sales_cal.R
@@ -0,0 +1,6 @@
+# This is my sales practice using R
+
+quarter1_sales <- 23232323.45
+quarter2_sales <- 3231414141.34
+midyear_sales <- quarter1_sales + quarter2_sales
+year_sales <- midyear_sales*2
